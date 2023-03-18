d424c9e0ba6c890a47b0bbd2a112222cc6958ee2
yoshizooo <y-i1996@yahoo.ne.jp>
2023-03-18 18:13:44 +0900

    修正４

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -38,7 +38,6 @@ class PrototypesController < ApplicationController
     if @prototype.update(prototype_params)
        redirect_to prototype_path
     else
-      @prototype = Prototype.new(prototype_params)
       render :edit
     end
   end
